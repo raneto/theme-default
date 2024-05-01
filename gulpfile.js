4ec20af4393fb88689ea01ebe0af32992af85251
Ryan Lelek <ryanlelek@gmail.com>
2024-04-30 21:57:49 -0500

    Dependencies: Upgrading gulp from v4.0.2 to v5.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,16 @@ var minify_css = require('gulp-clean-css');
 gulp.task('vendor', function () {
 
   var source = [
-    './node_modules/jquery/dist/**/*',
-    './node_modules/bootstrap/dist/**/*',
-    './node_modules/@popperjs/core/dist/**/*',
+    './node_modules/jquery/dist/**/*.min.js',
+    './node_modules/bootstrap/dist/css/**/*.min.css',
+    './node_modules/bootstrap/dist/js/**/bootstrap.min.js',
+    './node_modules/@popperjs/core/dist/umd/**/*.min.js',
     './node_modules/bootstrap-rtl/dist/**/*',
-    './node_modules/fitvids/dist/**/*',
-    './node_modules/highlight.js/**/*',
-    './node_modules/masonry-layout/dist/**/*',
-    './node_modules/@fixhq/sweetalert2/dist/**/*',
-    './node_modules/jquery-backstretch/**/*'
+    './node_modules/fitvids/dist/**/*.min.js',
+    './node_modules/highlight.js/styles/**/*.min.css',
+    './node_modules/masonry-layout/dist/**/*.min.js',
+    './node_modules/@fixhq/sweetalert2/dist/**/*.min.*',
+    './node_modules/jquery-backstretch/*.min.js'
   ];
 
   //var dest = 'themes/default/public/lib';
@@ -57,7 +58,7 @@ gulp.task('js', function () {
 // Static Files
 gulp.task('static', function () {
   return gulp
-    .src('./src/static/**/*', { base : './src/static' })
+    .src('./src/static/**/*', { encoding : false, base : './src/static' })
     .pipe(gulp.dest('./dist/public'));
 });
 gulp.task('zocial-temp', function () {
